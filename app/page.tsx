559f3d709ce8b0467bbf9aadebf913ba275b855f
Hrithik <hrithikpandey786@gmail.com>
2024-11-22 00:05:03 +0530

    modified tailwind.config and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
     <div>
-      Hello World
+      <h1 className="text-red center">Hello World</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,25 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        brand: {
+          DEFAULT: "#FA7275",
+          100: "#EA6365",
+        },
+        red: "#FF7474",
+        blue: "#56B8FF",
+        error: "#b80000",
+        green: "#3DD9B3",
+        orange: "#F9AB72",
+        light: {
+          100: "#333F4E",
+          200: "#A3B2C7",
+          300: "#F2F5F9",
+          400: "#F2F4F8"
+        },
+        dark: {
+          100: "#04050C",
+          200: "#131524"
+        }
       },
     },
   },
